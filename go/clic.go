ae22c5f2ef5cf1be4604fca20c1a02636871d6f3
Martin Disibio <mdisibio@qsrautomations.com>
2020-03-31 21:58:55 -0400

    Add upgrade command

--- go/clic.go
+++ go/clic.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -358,6 +359,7 @@ func doHelp(_ []string) error {
 	fmt.Println("  run        Run a command explicitly without a shell alias")
 	fmt.Println("  uninstall  Uninstall command")
 	fmt.Println("  unlink     Delete a shell alias")
+	fmt.Println("  upgrade    Upgrade installed command to the latest version")
 	fmt.Println()
 	fmt.Println("Run 'clic COMMAND --help' for more information on a command.")
 
@@ -409,6 +411,74 @@ func doFetch(args []string) error {
 	return fetch()
 }
 
+func doUpgrade(args []string) error {
+	parser := flag.NewFlagSet("upgrade", flag.ExitOnError)
+	parser.Usage = func() {
+		fmt.Println("Usage:  clic upgrade COMMAND")
+		parser.PrintDefaults()
+	}
+	if err := parser.Parse(args); err == flag.ErrHelp || len(args) < 1 {
+		parser.Usage()
+		return nil
+	}
+
+	cmdVers := parseCommand(parser.Arg(0))
+	if cmdVers.hasVersion {
+		return errors.New("Cannot specify version on upgrade. Try upgrading only ")
+	}
+
+	repo, err := loadRepo()
+	if err != nil {
+		return err
+	}
+
+	highestKnown := repo.resolveLatest(cmdVers)
+	if highestKnown == nil {
+		return fmt.Errorf("Unknown command: %s", parser.Arg(0))
+	}
+	highestKnownParsed := parseCommand(highestKnown.Name)
+
+	data, err := loadData()
+	if err != nil {
+		return err
+	}
+
+	highestInstalled := data.resolveLatest(cmdVers)
+	if highestInstalled != nil && highestInstalled.Name == highestKnown.Name {
+		return fmt.Errorf("Latest version %s already installed", highestInstalled.Name)
+	}
+
+	// Install highest
+	err = data.installCommand(*highestKnown)
+	if err != nil {
+		return err
+	}
+
+	err = link(highestKnownParsed)
+	if err != nil {
+		return err
+	}
+
+	// Uninstall older
+	cmds := data.sortedCommands()
+	for _, c := range cmds {
+		x := parseCommand(c)
+		if x.command == highestKnownParsed.command && x.version < highestKnownParsed.version {
+			err = data.uninstallCommand(x)
+			if err != nil {
+				return err
+			}
+
+			err = unlink(x)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 func main() {
 
 	processName := filepath.Base(os.Args[0])
@@ -446,6 +516,8 @@ func main() {
 		f = doUninstall
 	case "unlink":
 		f = doUnlink
+	case "upgrade":
+		f = doUpgrade
 	default:
 		f = doHelp
 	}
